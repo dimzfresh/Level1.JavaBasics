c59e0cf97560be42886f934470b136c7db78e922
Dimitrii Ziablikov <dimzfresh@gmail.com>
2023-05-01 23:26:29 +0300

    Level1.JavaBasics: Finish homework 1

---
+++ Level1.JavaBasics/src/JavaBasics.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public final class JavaBasics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Расскажите о вашем друге.\nКак зовут вашего друга?");
+        String name = scanner.nextLine();
+        System.out.println("Сколько лет вашему другу?");
+        int age = scanner.nextInt();
+        System.out.println("Моему другу " + name + " сейчас " + age + " лет");
+    }
+}
